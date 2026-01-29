ac016217caa25ec3f7643e28f8cad91c00ed235a
dhruvparmar25 <parmardhruv034@gmail.com>
2026-01-29 19:15:37 +0530

    Update grid gap in CommonProcessSection and adjust image width in CollaborationSection for improved layout

--- src/sections/contact/MapSection.jsx
+++ src/sections/contact/MapSection.jsx
@@ -1,6 +1,6 @@
 export default function MapSection() {
   return ( 
-    <section className=" py-16 px-6 xl:px-30 " >
+    <section className=" px:py-16 p-6 xl:px-30 " >
 
       <div className="w-full h-70 md:h-85 overflow-hidden">
 
